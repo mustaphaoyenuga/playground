6c52246bcc3869bde0819538c8c98444f2e88913
Mustapha Oyenuga <musty1k@yahoo.com>
2026-01-05 17:28:22 +0100

    feat: setup count functionality for characters, word and sentences

--- src/app/(pages)/character-counter/page.tsx
+++ src/app/(pages)/character-counter/page.tsx
@@ -6,26 +6,29 @@ import Logo from "./components/Logo";
 import StatCard from "./components/StatCard";
 import { Progress } from "@/components/ui/progress";
 
-const stats = [
-  {
-    count: "230",
-    label: "Total Characters",
-    className: "bg-purple-400"
-  },
-  {
-    count: "99",
-    label: "Word Count",
-    className: "bg-orange-400/90"
-  },
-  {
-    count: "00",
-    label: "Sentence Count",
-    className: "bg-red-400/90"
-  },
-]
 
 export default function CharacterCounter() {
+  const [stats, setStats] = useState({
+    totalCharacters: 0,
+    wordCount: 0,
+    sentenceCount: 0,
+    readingTime: 0,
+  });
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+    setStats((prevStats) => {
+      return {
+        ...prevStats,
+        totalCharacters: e.target.value.length,
+        wordCount: e.target.value.trim().split(" ").length,
+        sentenceCount: e.target.value.split('.').length
+      }
+    })
+  }
+
   return (
     <div className={isDarkMode ? "dark" : ""}>
       <div className="min-h-screen bg-background text-foreground">
@@ -36,7 +39,7 @@ export default function CharacterCounter() {
         <main className="max-w-4xl mx-auto">
           <h1 className="text-5xl md:text-7xl font-bold max-w-2xl text-center mx-auto">Analyze your text in real-time</h1>
           <section className="space-y-4">
-            <textarea className="w-full h-48 rounded-lg bg-muted p-2 resize-none"></textarea>
+            <textarea onChange={handleTextChange} value={text} className="w-full h-48 rounded-lg bg-muted p-2 resize-none"></textarea>
             <div className="flex items-center gap-6">
               <div className="flex items-center gap-2">
                 <input type="checkbox" />
@@ -53,9 +56,9 @@ export default function CharacterCounter() {
           </section>
 
           <section className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-12">
-            {stats.map((stat) => (
-              <StatCard key={stat.label} count={stat.count} label={stat.label} className={stat.className} />
-            ))}
+            <StatCard count={String(stats.totalCharacters).padStart(2, '0')} label="Total Characters" className="bg-purple-400" />
+            <StatCard count={String(stats.wordCount).padStart(2, '0')} label="Word Count" className="bg-orange-400/90" />
+            <StatCard count={String(stats.sentenceCount).padStart(2, '0')} label="Sentence Count" className="bg-red-400/90" />
           </section>
 
           <section>
